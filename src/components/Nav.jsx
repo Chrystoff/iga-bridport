7a5d548a0066deb0abd69f59ab4cc82d159079b2
Brian <10380@coderacademy.edu.au>
2023-01-03 19:39:50 +1100

    fixed hamburger menu with reacthook

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,64 +1,64 @@
+import React from "react";
+
 export default function Nav() {
+    const [isActive, setisActive] = React.useState(false);
+
     return (
     <>    
-        <nav class="navbar" role="navigation" aria-label="main navigation">
+        <nav class="navbar is-info" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                <a
+                    class="navbar-item" href="#">
+                    <img src="../../public/iga_logo.svg" alt="Failed to load image"/>
                 </a>
-
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
+                <a
+                    onClick={() => {
+                        setisActive(!isActive);
+                    }}
+                    role="button"
+                    class={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navMenu"
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
                 </a>
             </div>
-
-            <div id="navbarBasicExample" class="navbar-menu">
+            <div id="navMenu" class={`navbar-menu ${isActive ? "is-active" : ""}`}>
                 <div class="navbar-start">
-                <a class="navbar-item">
-                    Home
-                </a>
-
-                <a class="navbar-item">
-                    Documentation
-                </a>
-
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                    More
-                    </a>
-
-                    <div class="navbar-dropdown">
                     <a class="navbar-item">
-                        About
+                        Home
                     </a>
                     <a class="navbar-item">
-                        Jobs
+                        The Team
                     </a>
                     <a class="navbar-item">
-                        Contact
+                        Where we are
                     </a>
-                    <hr class="navbar-divider"/>
-                    <a class="navbar-item">
-                        Report an issue
-                    </a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            More
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item">
+                                About
+                            </a>
+                            <a class="navbar-item">
+                                Jobs
+                            </a>
+                            <a class="navbar-item">
+                                Contact
+                            </a>
+                            <hr class="navbar-divider"/>
+                            <a class="navbar-item">
+                                Report an issue
+                            </a>
+                        </div>
                     </div>
                 </div>
-                </div>
-
-                <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
-                    </div>
-                </div>
-                </div>
+                <div class="navbar-end"></div>
             </div>
         </nav>
     </>
